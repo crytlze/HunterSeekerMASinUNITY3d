549d97599bc971c155138ce503c2316c84674e09
Muhammad Sufi "CRYTLZE" Aulia <41355094+crytlze@users.noreply.github.com>
2023-03-19 22:54:29 +0700

    VER 1.0
    Hunter Seeker Agen A, B dan C

---
+++ Assets/Script/AgentController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    public int numberAgentA;
+    public int numberAgentB;
+    public int numberAgentC;
+
+    public GameObject prefabAgentA;
+    public GameObject prefabAgentB;
+    public GameObject prefabAgentC;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 size = transform.localScale;
+
+        for (int i = 0; i < numberAgentA; i++)
+        {
+            GameObject instanceAgentA = (GameObject)Instantiate(prefabAgentA);
+            instanceAgentA.transform.position = new Vector3(Random.Range(-size.x * 10f, size.x * 10f), 0, Random.Range(-size.z * 10f, size.z * 10f));
+
+        }
+
+        for (int i = 0; i < numberAgentB; i++)
+        {
+            GameObject instanceAgentB = (GameObject)Instantiate(prefabAgentB);
+            instanceAgentB.transform.position = new Vector3(Random.Range(-size.x * 10f, size.x * 10f), 0, Random.Range(-size.z * 10f, size.z * 10f));
+
+        }
+
+        for (int i = 0; i < numberAgentC; i++)
+        {
+            GameObject instanceAgentC = (GameObject)Instantiate(prefabAgentC);
+            instanceAgentC.transform.position = new Vector3(Random.Range(-size.x * 10f, size.x * 10f), 0, Random.Range(-size.z * 10f, size.z * 10f));
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
